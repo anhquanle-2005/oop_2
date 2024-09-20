0d7c55456a8db079569325a2ee3df9a0e4423d08
22.Lê Gia Anh Quân <legiaanhquan98563210@gmail.com>
2024-09-20 08:00:57 +0700

    \hh

---
+++ tuan3/bai6.java
@@ -0,0 +1,49 @@
+package tuan3;
+
+import java.util.Scanner;
+
+public class bai6 
+{
+    public static void main(String[] args) 
+    {   
+            int a,b;
+            char dau;
+            Scanner sc= new Scanner(System.in);
+            System.out.println("nhap vao a: ");
+            a=sc.nextInt();
+            System.out.println("nhap vao b:");
+            b=sc.nextInt();
+            System.out.println("nhap dau vao:");
+            dau = sc.next().charAt(0);
+            switch (dau) 
+            {
+                case '+':
+                    int tong;
+                    tong=a+b;
+                    System.out.println(a+" + "+b +" = "+tong);
+                    break;
+                case '-':
+                    int hieu;
+                    hieu=a-b;
+                    System.out.println(a+" - "+b +" = "+hieu);
+                    break;
+                case '*':
+                    int tich;
+                    tich=a*b;
+                    System.out.println(a+" * "+b +" = "+tich);
+                    break;
+                case '/':
+                if(b!=0)
+                    {
+                        float thuong;
+                        thuong= (float)a/b;
+                        System.out.println(a+" / "+b +" = "+thuong);
+                    }
+                else 
+                    System.out.println("khong tinh duoc ");
+                default:
+                    System.out.println("nhap sai");
+            }
+            
+    }    
+}
