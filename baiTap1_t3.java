0aabcea598e1095d17cef731db7e4bcec22e6087
le gia anh quan <23115053122133@sv.ute.udn.vn>
2024-09-22 21:08:52 +0700

    hoai nam 22 nop bai baTap1_t3

--- baiTap1_t3.java
+++ baiTap1_t3.java
@@ -2,7 +2,8 @@ package tuan3;
 
 import java.util.*;
 public class baiTap1_t3{
-       public static void main(String[] args) {
+       public static void main(String[] args) 
+    {
         int a;
         Scanner sc = new Scanner(System.in);
         System.out.println("nhap vao mot so : ");
