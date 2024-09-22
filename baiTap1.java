7213e0b73d218bc60ac3d1e31045223642b24435
Nguyen Hoai Nam <hoainamn488@gmail.com>
2024-09-22 13:54:11 +0700

    hnam22

---
+++ baiTap1.java
@@ -0,0 +1,24 @@
+package tuan3;
+
+import java.util.*;
+public class baiTap1 {
+       public static void main(String[] args) {
+        int a;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("nhap vao mot so : ");
+        a = sc.nextInt();
+        KTRA(a);
+    }
+    public static void KTRA(int x)
+    {
+        if(x==0)
+            System.out.println("so "+x+" la so 0");
+        else
+            if(x>0)
+                System.out.println("so "+x+" la so duong");
+            else
+                System.out.println("so "+x+" la so am");
+    }
+
+    
+}
