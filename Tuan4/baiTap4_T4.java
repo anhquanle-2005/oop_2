dfe0319d5fcbeb9b71fb1d74c5b705b705d5686c
Nguyen Hoai Nam <hoainamn488@gmail.com>
2024-09-23 12:22:47 +0700

    hnam22 nop bai tuan 4

---
+++ Tuan4/baiTap4_T4.java
@@ -0,0 +1,28 @@
+
+import java.util.*;
+public class baiTap4_T4 {
+
+   public static void main(String[] args) 
+    {
+        int a;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("nhap vao mot so nguyen:");
+        a = sc.nextInt();
+        KtraSoNguyenTo(a);
+    }
+    public static void KtraSoNguyenTo(int x)
+    {
+        int i,s;
+        s=0;
+        if(x==1)
+        s=1;
+        else
+        for(i=1;i<=x;i++)
+            if(x%i==0)
+                s=s+1;
+        if(s==2)
+        System.out.println(x+" la so nguyen to");
+        else
+        System.out.println(x+" khong phai la so nguyen to");
+    }
+}
