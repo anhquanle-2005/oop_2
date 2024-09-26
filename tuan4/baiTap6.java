4502208a75e5db8a207107608635f0a008961a85
23115053122133@sv.ute.udn.vn <23115053122133@sv.ute.udn.vn>
2024-09-26 22:19:51 +0700

    xoa file de

--- tuan4/baiTap6.java
+++ tuan4/baiTap6.java
@@ -1,10 +1,30 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class baiTap6 
 {
-    
+    static int n;
+    static Scanner sc;
     public static void main(String[] args) 
     {
-            
-    }    
+        Scanner sc =new Scanner(System.in);
+        System.out.println("nhap vao so n = ");
+        n=sc.nextInt();
+        doiXung();
+    }
+
+    static void doiXung()
+    {
+        int a=n, dao=0;
+        while (n!=0) 
+        {
+            int tong=n%10;
+            dao=dao*10+tong;
+            n=n/10;   
+        }
+        if (dao==a) 
+            System.out.println("so nay doi xung");
+        else 
+            System.out.println("so nay khong doi xung");
+
+    }
 }
