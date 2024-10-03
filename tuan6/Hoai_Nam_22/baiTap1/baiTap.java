d83b32a7745c4c30d9b9a1e160a4125aa4d862d4
23115053122133@sv.ute.udn.vn <23115053122133@sv.ute.udn.vn>
2024-10-03 21:23:57 +0700

    chinh ten file tuan 6

---
+++ tuan6/Hoai_Nam_22/baiTap1/baiTap.java
@@ -0,0 +1,11 @@
+package tuan6.Hoai_Nam_22.baiTap1;
+
+public class baiTap {
+    public static void main(String[] args) 
+    {
+        hinhChuNhat hcn = new hinhChuNhat();
+        hcn.nhap();
+        hcn.xuat();
+    }
+    
+}
